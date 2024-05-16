dae91bd337616628529ed9de6c6e695eb00dcd25
Kin <150116082+Kinema233@users.noreply.github.com>
2024-05-16 11:46:32 +0300

    Add files via upload

---
+++ Prakt_8/Prakt_8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Prakt_8
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Введіть текст: ");
+            string inputText = Console.ReadLine();
+
+            string pattern = @"\b\d{2}[-./]\d{2}[-./]\d{4}\b";
+
+            Regex regex = new Regex(pattern);
+
+            Match match = regex.Match(inputText);
+
+            if (match.Success)
+            {
+                Console.WriteLine("Перша знайдена дата в тексті:");
+                Console.WriteLine(match.Value);
+            }
+            else
+            {
+                Console.WriteLine("Дати не знайдено в тексті.");
+            }
+
+            Console.WriteLine("Натисніть будь-яку клавішу для виходу.");
+            Console.ReadKey();
+        }
+    }
+}
